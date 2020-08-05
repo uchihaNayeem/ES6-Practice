bcdf4cfe2c6d96c1feff92ce8e137cc36d9cad49
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-08-06 02:21:09 +0600

    let const block scope

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const name = `Omar Sunny` // const is fixed, cant change it later
+const hubby = `Omar Sunny` // const is fixed, cant change it later
 
-console.log(name);
+console.log(hubby);
+
+
+const realAge = [12, 34, 67, 45, 25, 97];
+console.log(realAge);
+
+realAge[0] = 100;
+console.log(realAge);
+realAge.push(9999);
+console.log(realAge); // akhane const korar poreo index dhore change kora jay; abar push pop kora jay
+
+
+const actorsHollywood = {name: `Tom Hanks`, age: 18};
+console.log(actorsHollywood);
+actorsHollywood.age = 288;
+console.log(actorsHollywood);
